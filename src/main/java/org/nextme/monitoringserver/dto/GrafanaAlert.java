6981ab71ef0b3c8d627545cc2e36f5f73253ed2f
hoonssac <hoonssac.dev@gmail.com>
2025-12-17 11:18:46 +0900

    feat: Alert, Metric 수집 DTO 클래스 작성

---
+++ src/main/java/org/nextme/monitoringserver/dto/GrafanaAlert.java
@@ -0,0 +1,47 @@
+package org.nextme.monitoringserver.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class GrafanaAlert {
+
+	private String receiver;
+	private String status;
+	private List<Alert> alerts;
+
+	@JsonProperty("groupLabels")
+	private Map<String, String> groupLabels;
+
+	@JsonProperty("commonLabels")
+	private Map<String, String> commonLabels;
+
+	@JsonProperty("commonAnnotations")
+	private Map<String, String> commonAnnotations;
+
+	@JsonProperty("externalURL")
+	private String externalURL;
+
+	@Data
+	public static class Alert {
+		private String status;
+		private Map<String, String> labels;
+		private Map<String, String> annotations;
+
+		@JsonProperty("startsAt")
+		private String startsAt;
+
+		@JsonProperty("endsAt")
+		private String endsAt;
+
+		@JsonProperty("generatorURL")
+		private String generatorURL;
+
+		@JsonProperty("fingerprint")
+		private String fingerprint;
+	}
+}
